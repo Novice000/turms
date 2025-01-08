5d3b0060b1784bcb100d75d99e448029aaca856d
Novice000 <maxwellefemena@gmail.com>
2025-01-08 18:44:27 +0100

    added dockerfile to the frontend and backend folder and a docker-compose in the root folder

--- backend/turms/models.py
+++ backend/turms/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class UserAccount(AbstractUser):
+    choice = [("driver", "DRIVER"),("requester", "REQUESTER")]
+    email = models.EmailField(max_length=255)
+    first_name = models.CharField(max_length=255)
+    
